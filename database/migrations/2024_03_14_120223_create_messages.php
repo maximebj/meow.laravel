9816a93ba583eace37c374529764bec25345b91d
Maxime BJ <maxime@dysign.fr>
2024-03-14 13:21:46 +0100

    Migrations for Messages and Comments

---
+++ database/migrations/2024_03_14_120223_create_messages.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use HasUuids;
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('content', 300);
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
---
+++ database/migrations/2024_03_14_120634_create_comments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use HasUuids;
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('content', 300);
+            $table->foreignUuid('message_id')->references('id')->on('messages')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
